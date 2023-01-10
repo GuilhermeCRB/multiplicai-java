44af2eee55403764eee3c08505104ec48a01495b
Guilherme Barros <guilherme.barros@fazenda.rj.gov.br>
2023-01-10 14:52:27 -0300

    feat: creates project

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+
+        int num = 1;
+        System.out.println("Tabela de multiplicação de X");
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i * num);
+        }
+    }
+}
